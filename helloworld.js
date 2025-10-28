671e79f3d460e8b512303606aa3f66425487a076
73% Geek <73pctGeek@users.noreply.github.com>
2025-10-28 16:36:11 +0100

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,6 @@
 (function() {
 function printHelloWorld()
 {
-    console.log("Hello world from script hosted on
-    	github repo and deliverd through jsDelivr");
+    alert("Hello world from script hosted on github repo and deliverd through jsDelivr");
 }
 })();
